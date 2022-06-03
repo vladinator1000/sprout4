6182e3a845e22be92931fb0713d5fed7d7c391a3
Vlady Veselinov <vladkodmc@gmail.com>
2022-06-03 04:42:45 +0100

    initial commit

---
+++ Source/Sprout/SproutGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SproutGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPROUT_API ASproutGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
